76375855370529d460885e827b3e0e3c80b7f399
(Student  C00294882) Karol Loughlin <C00294882@setu.ie>
2023-11-17 09:33:08 +0000

    Added Karol's Starting Code

--- Converter/Converter/Converter.cpp
+++ Converter/Converter/Converter.cpp
@@ -7,3 +7,17 @@ int main()
 {
 	return 1;
 }
+
+char printMenu() // written by Karol
+{
+    char choice = ' ';// user input  choice
+    std::cout << "Please enter a upper case letter to convert from left to right and lower case to convert from right to left." << std::endl;
+
+    std::cout << "[D] Mpg---------<--->---L/100km        [d]" << std::endl;
+    std::cout << "[E] Kilograms---<--->---Stone & Pounds        [e]" << std::endl;
+    std::cout << "[F] Celsius---<--->---Fahrenheit     [f]" << std::endl;
+    std::cout << "[Q] Exit------[---]----Exit           [q]" << std::endl;
+    std::cout << "Enter your Choice : ";
+    std::cin >> choice;
+    return choice;
+}
