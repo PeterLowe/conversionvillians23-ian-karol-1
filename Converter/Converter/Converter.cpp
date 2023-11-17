d765c5531e7c8c6a5ba4fdc5e48630cd4dcbc9c5
(Student  C00294882) Karol Loughlin <C00294882@setu.ie>
2023-11-17 09:58:13 +0000

    fixed some issues

--- Converter/Converter/Converter.cpp
+++ Converter/Converter/Converter.cpp
@@ -2,9 +2,16 @@
 // Names: Ian Perez Bunuel - Karol Loughlin
 
 #include <iostream>
+//print menu added
+char printMenu();
+
+//Converters - Karol
+void celToFah();
 
 int main()
 {
+    char choice = printMenu();
+
 	return 1;
 }
 
@@ -18,15 +25,6 @@ char printMenu() // written by leader
 
     std::cout << "[Q] Exit        [---]   Exit           [q]" << std::endl;
     std::cout << "Enter your Choice : ";
-    std::cin >> choice;
-    return choice;
-}
-
-char printMenu() // written by Karol
-{
-    char choice = ' ';// user input  choice
-    std::cout << "Please enter a upper case letter to convert from left to right and lower case to convert from right to left." << std::endl;
-
     std::cout << "[D] Mpg---------<--->---L/100km        [d]" << std::endl;
     std::cout << "[E] Kilograms---<--->---Stone & Pounds        [e]" << std::endl;
     std::cout << "[F] Celsius---<--->---Fahrenheit     [f]" << std::endl;
@@ -34,4 +32,11 @@ char printMenu() // written by Karol
     std::cout << "Enter your Choice : ";
     std::cin >> choice;
     return choice;
+}
+
+void celToFah()
+{
+    float cel = 0.0f;
+    float fah = 0.0f;
+    const float CEL_TO_FAH = cel * 9 / 5 + 32;
 }
